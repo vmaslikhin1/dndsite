8830f35d6e7d73c3a2f2fec011e5e7fb47f5a2b9
Vadim Maslikhin <batvad11@gmail.com>
2021-01-11 03:21:22 +0300

    Update current_art.js

--- current_art.js
+++ current_art.js
@@ -3,11 +3,41 @@ var human_art = {
     "art2": "test2",
     "art3": "test3"
 }
+var goblin_art = {
+    "art1": "test",
+    "art2": "test2",
+    "art3": "test3"
+}
+var satyr_art = {
+    "art1": "test",
+    "art2": "test2",
+    "art3": "test3"
+}
+var dwarf_art = {
+    "art1": "test",
+    "art2": "test2",
+    "art3": "test3"
+}
 
 function h_upd() {
     document.getElementById("h_a1").innerHTML = JSON.stringify(human_art.art1)
     document.getElementById("h_a2").innerHTML = JSON.stringify(human_art.art2)
     document.getElementById("h_a3").innerHTML = JSON.stringify(human_art.art3)
+}
+function g_upd() {
+    document.getElementById("g_a1").innerHTML = JSON.stringify(goblin_art.art1)
+    document.getElementById("g_a2").innerHTML = JSON.stringify(goblin_art.art2)
+    document.getElementById("g_a3").innerHTML = JSON.stringify(goblin_art.art3)
+}
+function s_upd() {
+    document.getElementById("s_a1").innerHTML = JSON.stringify(satyr_art.art1)
+    document.getElementById("s_a2").innerHTML = JSON.stringify(satyr_art.art2)
+    document.getElementById("s_a3").innerHTML = JSON.stringify(satyr_art.art3)
+}
+function d_upd() {
+    document.getElementById("d_a1").innerHTML = JSON.stringify(dwarf_art.art1)
+    document.getElementById("d_a2").innerHTML = JSON.stringify(dwarf_art.art2)
+    document.getElementById("d_a3").innerHTML = JSON.stringify(dwarf_art.art3)
 }
 
 function h_save() {
